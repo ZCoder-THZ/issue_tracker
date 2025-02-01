5b7a51b6e8af930cae236d0be8ea0e98b2a4fb46
thanhtikezaw <thanhtikezaw919@gmail.com>
2025-02-01 22:52:59 +0700

    feats:socket url changed for deployment

--- src/app/issues/[id]/socket.ts
+++ src/app/issues/[id]/socket.ts
@@ -2,6 +2,6 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4001", {
+export const socket = io("http://13.250.57.111:4001", {
     autoConnect: false, // Prevent automatic connection
 });
